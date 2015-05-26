4e0774906f56da4cb245b969ce5ed0802aedde59
Thomas Gebert <thomasgebert@gmail.com>
2015-05-26 01:09:26 -0400

    Get the algorithm actually working.
    For a reason I don't fully understand yet, we need to make sure that we
    make the new-cell list unique.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,11 @@
 module Main where 
 
+
+
+--unique :: [Int] -> [Int]
+unique xs = [x | (x,y) <- zip xs [0..], x `notElem` (take y xs)]
+
+
 getPossibleAdjacent :: (Integral a) => (a,a) -> [(a,a)]
 getPossibleAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1)], not ((xa, ya) == (x,y))]
  
@@ -18,10 +24,10 @@ getNonlivingAdjacents = nonLivingFilter . getPossibleAdjacent
 nonlivingAdjacentCount x xs = length $ getNonlivingAdjacents x xs
 
 
-newSet x = remainingLiveCells ++ spawnedDeadCells
+newSet x = remainingLiveCells ++ (unique spawnedDeadCells)
       where 
-         remainingLiveCells = [x' | x' <- x, (livingAdjacentCount x' x) > 2 && (livingAdjacentCount x' x) <= 3]
-         spawnedDeadCells = [x' | x' <- x, (nonlivingAdjacentCount x' x) == 3]
+         remainingLiveCells = [x' | x' <- x, (livingAdjacentCount x' x) == 2 || (livingAdjacentCount x' x) == 3]
+         spawnedDeadCells = [y' | x' <- x, y' <- (getNonlivingAdjacents x' x), (livingAdjacentCount y' x) == 3]
 
 main :: IO ()
 main = putStrLn "hellojkk"
