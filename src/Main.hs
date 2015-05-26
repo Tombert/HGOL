732b66b21d4986c18a4a10f89f649161ecc5329c
Thomas Gebert <thomasgebert@gmail.com>
2015-05-25 22:30:59 -0400

    Add type signature for the living adjacent fucntions.

--- src/Main.hs
+++ src/Main.hs
@@ -6,6 +6,7 @@ getPossibleAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1
 filterAdjacent :: (Integral a) => [(a, a)] -> [(a,a)] -> [(a,a)]
 filterAdjacent x y = [ x' | x' <- x, x' `elem` y]
 
+getLivingAdjacents :: (Integral a) => (a,a) -> [(a,a)] -> [(a,a)]
 getLivingAdjacents = filterAdjacent . getPossibleAdjacent
 
 
