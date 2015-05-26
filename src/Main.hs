10ccf2a0b8bdcc8ca043e67ad7fec0850b4ac807
Thomas Gebert <thomasgebert@gmail.com>
2015-05-25 22:45:17 -0400

    Abstract out living and non-living using a higher-order function.

--- src/Main.hs
+++ src/Main.hs
@@ -3,11 +3,16 @@ module Main where
 getPossibleAdjacent :: (Integral a) => (a,a) -> [(a,a)]
 getPossibleAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1)], not ((xa, ya) == (x,y))]
  
-filterAdjacent :: (Integral a) => [(a, a)] -> [(a,a)] -> [(a,a)]
-filterAdjacent x y = [ x' | x' <- x, x' `elem` y]
+filterAdjacent :: (Integral a) => ((a,a) -> [(a,a)] -> Bool) -> [(a, a)] -> [(a,a)] -> [(a,a)]
+filterAdjacent f x y = [ x' | x' <- x, f x' y]
 
-getLivingAdjacents :: (Integral a) => (a,a) -> [(a,a)] -> [(a,a)]
-getLivingAdjacents = filterAdjacent . getPossibleAdjacent
+livingFilter = filterAdjacent elem
+nonLivingFilter = filterAdjacent notElem
+
+getLivingAdjacents = livingFilter . getPossibleAdjacent
+
+getNonlivingAdjacents = nonLivingFilter . getPossibleAdjacent
+
 
 
 main :: IO ()
