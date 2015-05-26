a4960e63669b9d42bc38f7b8fa3b995a254b2218
Thomas Gebert <thomasgebert@gmail.com>
2015-05-25 22:04:45 -0400

    Add function to get adjacent tuples

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,7 @@
 module Main where 
 
+getAdjacent :: (Integral a) => (a,a) -> [(a,a)]
+getAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1)], not ((xa, ya) == (x,y))]
+            
 main :: IO ()
 main = putStrLn "hello"
