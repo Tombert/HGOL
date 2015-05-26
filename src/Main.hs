47a271eb89cb7736ce576fe584a016fb02ba6d79
Thomas Gebert <thomasgebert@gmail.com>
2015-05-26 15:49:53 -0400

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,11 @@
 module Main where 
 
+-- This grabs all the cells that are adjacent cells to a supplied cell, making sure to not include that cell. 
+-- Thank goodness tuples implement Eq. 
 getPossibleAdjacent :: (Integral a) => (a,a) -> [(a,a)]
 getPossibleAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1)], not ((xa, ya) == (x,y))]
- 
+
+-- This simply filters down all the cells that return false in the supplied function. 
 filterAdjacent :: (Integral a) => ((a,a) -> [(a,a)] -> Bool) -> [(a, a)] -> [(a,a)] -> [(a,a)]
 filterAdjacent f x y = [ x' | x' <- x, f x' y]
 
