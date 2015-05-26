06bc0a189e4557e3712e3d30a6bf9dde2d0166ba
Thomas Gebert <thomasgebert@gmail.com>
2015-05-25 22:22:33 -0400

    write system to get all living cells.

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,13 @@
 module Main where 
 
-getAdjacent :: (Integral a) => (a,a) -> [(a,a)]
-getAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1)], not ((xa, ya) == (x,y))]
-            
+getPossibleAdjacent :: (Integral a) => (a,a) -> [(a,a)]
+getPossibleAdjacent (x,y) = [(xa, ya) | xa <- [(x-1)..(x+1)], ya <- [(y-1)..(y+1)], not ((xa, ya) == (x,y))]
+ 
+filterAdjacent :: (Integral a) => [(a, a)] -> [(a,a)] -> [(a,a)]
+filterAdjacent x y = [ x' | x' <- x, x' `elem` y]
+
+getLivingAdjacents = filterAdjacent . getPossibleAdjacent
+
+
 main :: IO ()
 main = putStrLn "hello"
